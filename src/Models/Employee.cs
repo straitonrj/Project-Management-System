e68b16626010ff609900c35fc36d28beea872b18
RJ Straiton <straitonrj@gmail.com>
2025-04-09 15:01:48 -0400

    Uploaded current work on the Models. Still a work in progress, especially Project.cs. Also created a new branch for the models

---
+++ src/Models/Employee.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace src.Models;
+
+public class Employee
+{
+    private int ID { get; set; }
+    private string Name { get; set; }
+    private string JobTitle { get; set; }
+
+    public Employee(int tempID, string tempName, string tempJob)
+    {
+        ID = tempID;
+        Name = tempName;
+        JobTitle = tempJob;
+    }
+
+    public string ToString()
+    {
+        return "ID: "+ID+" Name: "+Name+" Position: "+JobTitle;
+    }
+}
